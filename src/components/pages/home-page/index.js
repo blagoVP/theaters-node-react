31aee5b9cbb93421787edd1d51d4ca5ec010fb5d
bpetrov <vpblagovest@gmail.com>
2020-08-05 18:06:24 +0300

    Connected Home page with the api

--- src/components/pages/home-page/index.js
+++ src/components/pages/home-page/index.js
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PageLayout from '../../page-layout';
 import PlaysGuest from '../../plays-guest';
 import PlaysUser from '../../plays-user';
 
-const HomePage = () => {
-  return (
-    <div>
-      <PageLayout>
-      <PlaysGuest />
-      </PageLayout>
-    </div>
-  )
+class HomePage extends Component {
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      plays: []
+    }
+  }
+
+
+
+  componentDidMount(){
+    fetch('http://localhost:9999/api/home/').then((res) =>{
+      res.json().then((data)=>{
+        const plays = data.slice(0, 3)//this should be removed, after api get fixed
+        this.setState({
+          plays
+        })
+        console.log(this.state.plays)
+      })
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <PageLayout>
+          <PlaysGuest />
+        </PageLayout>
+      </div>
+    )
+  }
 }
 
 export default HomePage;
